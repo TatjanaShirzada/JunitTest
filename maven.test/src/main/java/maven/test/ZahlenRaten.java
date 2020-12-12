584c09c6aa743ea08bf4f64498d43574dc487746
TatjanaShirzada <75101162+TatjanaShirzada@users.noreply.github.com>
2020-12-13 00:30:09 +0100

    Add files via upload

---
+++ maven.test/src/main/java/maven/test/ZahlenRaten.java
@@ -0,0 +1,35 @@
+package maven.test;
+
+import java.util.Scanner; 
+
+
+public class ZahlenRaten {
+	
+	static void rateZahl(int wertebereich){
+		int zufallsZahl = (int) (Math.random()*wertebereich); 
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Errate die Zahl zwischen 0 und "+wertebereich+" !");
+		int eingabe = scanner.nextInt(); //Eingabe gespeichert
+		int zaehler=1; //ZŠhler startet bei 1 
+		
+		while (zufallsZahl != eingabe){
+			if (zufallsZahl<eingabe){
+				System.out.println("Leider nein, die Zahl ist kleiner. Du hattest "+zaehler+" Versuche gehabt. Versuche es nochmal!");
+			}
+			else {
+				System.out.println("Leider nein, die Zahl ist groesser, du hattest "+zaehler+" Versuche gehabt. Versuche es nochmal!");
+			}
+			
+			eingabe = scanner.nextInt(); //erneute Eingabe
+			zaehler++; //ZŠhler bei jedem Durchlauf um eins erhšht
+		}
+		System.out.println("Bingo! Du hattest " +zaehler+ " Durchgaenge gebraucht"); //Eingabe war richtig
+	}
+	
+	public static void main(String[] args) {
+		rateZahl(100);
+	}
+
+}
---
+++ maven.test/src/test/java/maven/test/ProgramTest.java
@@ -0,0 +1,21 @@
+package maven.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProgramTest {
+
+
+	@Test
+	public void test() {
+		int a =1;
+		int b=2;
+		assertTrue( a + b == 3);
+	}
+	
+
+
+}
+
+
